3e303bdd45e07e72dd95e78694a09ba983983bad
Danielle Goldberg <dhgoldberg15@gmail.com>
2024-01-06 18:20:27 +0000

    lowdown doc

--- lowdown/index.js
+++ lowdown/index.js
@@ -61,11 +61,11 @@ function typeOf(value){
 
 module.exports.each = typeOf;
 /**
- * first: The function take in an array and a number. It first determons that the number is a number, it is a number
+ * first: The function take in an array and a number. It first determons that the number is a number, if it is a number
  * it returns the zero index of the array. If input is a number less than zero, it returns and empty array
- * @param {*} array 
- * @param {*} number 
- * @returns 
+ * @param {*} array takes in an input array
+ * @param {*} number takes in a number to compair against 
+ * @returns an empty array, first index of the array or array indxis equle to the number.
  */
 
  function first(array, number){
@@ -82,3 +82,266 @@ module.exports.each = typeOf;
     }
 
 }
+module.exports.each = first;
+/**
+ * last: The function takes in an array and number. It returns 
+ * @param {*} array 
+ * @param {*} number 
+ * @returns 
+ */
+
+ function last(array, number){
+    if(Array.isArray(array)=== false){
+        return [];
+    } if (number === undefined || number === NaN){
+        return array [array.length -1];
+    } if (number < 0){
+        return []
+    }if (number > array.length){
+        return array
+    } else {
+        return array.slice(1, array[number.length -1])
+    }
+}
+module.exports.each = last;
+/**
+ * 
+ * @param {*} array 
+ * @param {*} value 
+ * @returns 
+ */
+
+ function contains(array, value){
+    for (let i =0; i < array.length; i++){
+        if (array[i] === value){
+            return true;
+        }
+    } return false
+}
+module.exports.each = contains;
+/**
+ * 
+ * @param {*} collection 
+ * @param {*} func 
+ */
+
+_.each = function (collection, func){
+    for( let i = 0; i < collection.length; i++){
+        if (Array.isArray(collection)=== false){
+            func(collection[i], i, collection);
+        }
+    }
+    for (let key in collection){
+        if (typeof collection === 'object'){
+            func(collection[key], key, collection);
+        
+        }
+    }
+}
+module.exports.each = each;
+/**
+ * 
+ * @param {*} array 
+ * @returns 
+ */
+ function unique(array){
+    let uniqueArray = []
+    for(let i = 0; i < array.length; i++ ){
+        if(uniqueArray.indexOf(array[i])=== -1){
+            uniqueArray.push(array[i])
+        }
+    }
+   
+
+    return  uniqueArray;
+}module.exports.each = unique;
+/**
+ * 
+ * @param {*} array 
+ * @param {*} func 
+ * @returns 
+ */
+  function filter (array,func){
+    let newArray = []
+    _.each (array, function(element, index, array){
+        if(func(element, index, array)){
+            newArray.push(element);
+        }
+    });
+    return newArray;
+}
+module.exports.each = filter;
+/**
+ * 
+ * @param {*} array 
+ * @param {*} func 
+ * @returns 
+ */
+function reject(array, func){
+    let newArray = [];
+    _.each(array, function (element, index, array){
+        if(func(element, index, array)=== false){
+            newArray.push(element);
+        }
+    });
+    return newArray
+} 
+
+module.exports.each = reject;
+
+ function partition(array, func){
+    let truthy = []
+    let falsy = []
+    for (let i = 0; i < array.length; i++){
+        if(func(array[i], i, array)){
+            truthy.push(array[i]);
+        } else {
+            falsy.push(array[i]);
+        }
+    }
+    return [truthy, falsy]
+}
+module.exports.each = partition;
+/**
+ * 
+ * @param {*} collection 
+ * @param {*} func 
+ * @returns 
+ */
+ function map(collection, func){
+    // create an ouput array
+    const output = []
+    // if collection is an array use a for loop
+    if (Array.isArray(collection)){
+        for (let i = 0; i < collection.length; i++ ){
+            // invoke callback and store in a variable 
+            // call the callback function 
+            let result = func(collection[i], i, collection)
+            output.push(result);
+    }
+    } else {
+        // if collection is an object use a for in loop 
+        for( let key in collection){
+            let result = func(collection[key], key, collection);
+            output.push(result)
+        }
+    }
+
+    return output;
+
+}
+module.exports.each = map;
+/**
+ * 
+ * @param {*} array 
+ * @param {*} key 
+ * @returns 
+ */
+
+function pluck(array, key){
+    let object = _.map(array, function(object){
+        return object[key];
+    });
+    return object;
+}
+
+module.exports.each = pluck;
+/**
+ * 
+ * @param {*} collection 
+ * @param {*} func 
+ * @returns 
+ */
+
+ function every(collection, func){
+    if(func === undefined){
+        if (Array.isArray(collection)){
+        // for loop
+            for (var i = 0; i < collection.length; i++){
+        // determon if current item is NOT truthy 
+            if(!collection[i]){
+             return false; 
+            }
+        }
+    
+    } else { 
+        for (var key in collection){
+         if(!collection[key]){
+            return false; 
+         }
+        }
+    }
+    } else {
+        if(Array.isArray(collection)){
+            for (var i = 0; i < collection.length; i++){
+                if(!func(collection[i], i, collection)){
+                    return false;
+                }
+            }
+        } else {
+            for (var key in collection){
+                if(!func(collection[key], key, collection)){
+                    return false; 
+                }
+            }
+        }
+    }
+        return true;
+    } 
+    module.exports.each = every;
+    /**
+     * 
+     * @param {*} collection 
+     * @param {*} func 
+     * @returns 
+     */
+    function some(collection, func){
+        if(func === undefined){
+            if(Array.isArray(collection)){
+                for (let i = 0; i < collection.length; i++){
+                    if (collection[i]){
+                        return true;
+                    }
+                }
+            } else {
+                for (let key in collection){
+                    if (collection[key]){
+                        return true;
+                    }
+                }
+            }
+        }else {
+            if (Array.isArray(collection)){
+                for(let i = 0; i < collection.length; i++){
+                    if(func(collection[i], i, collection === true)){
+                        return true;
+                    }
+                }
+            } else {
+                for (let key in collection){
+                    if (func(collection[key], key, collection) === true){
+                        return true
+                    }
+                }
+            }
+        } return false;
+        } 
+        module.exports.each = some;
+
+         function reduce(array, func, seed){
+            // inishalize an output variable 
+            let output;
+            if(seed === undefined){
+                output = array[0];
+                for (let i = 1; i < array.length; i++){
+                    output = func(output, array[i], i)
+                }
+            } else {
+                output = seed;
+                for (let i = 0; i < array.length; i++){
+                    output = func(output, array[i], i)
+                }
+            }
+            return output;
+            }
+            module.exports.each = reduce;
