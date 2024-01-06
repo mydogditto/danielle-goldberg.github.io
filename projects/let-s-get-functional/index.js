bd56efb4cc2c0cf522ce32211f8196fc10c33c3a
Danielle Goldberg <dhgoldberg15@gmail.com>
2024-01-06 21:35:38 +0000

    lets get functinal

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -3,7 +3,7 @@
 'use strict';
 
 var customers = require('./data/customers.json');
-var _ = require(/* Replace this with the name of your lodown! */);
+var _ = require("underbar");
 
 /**
  * 1. Import your lodown module using the require() method,
@@ -16,7 +16,7 @@ var _ = require(/* Replace this with the name of your lodown! */);
  *
  * 4. To test your work, run the following command in your terminal:
  *
- *    npm start --prefix ./<YOUR_GITHUB_FOLDER/projects/let-s-get-functional
+ *    npm start --prefix ./danielle-goldberg.github.io/projects/let-s-get-functional
  *
  *    IMPORTANT: Make sure you replace <YOUR_GITHUB_FOLDER with your actual github folder name that is in your workspace.
  */
@@ -24,23 +24,48 @@ var _ = require(/* Replace this with the name of your lodown! */);
 var maleCount = function(array) {
 //loop through the array and use filter to output the number of male 
 //customers
+let males = _.filter(array, function(customer){
+    return customer.gender === 'male';
+});
 };
 
 var femaleCount = function(array){
-//find the number of female customers using reduce output number
-};
+// //find the number of female customers using reduce output number
+//     return _.reduce(array, function(accumulator, current){
+//         if (current.gender === "female"){
+//             accumulator += 1;
+//         }
+//         return accumulator;
+//     }, 0)
+}
 
 var oldestCustomer = function(array){
-    // loop through the array to fine the person with the oldest age value.
+    // loop through the array to find the person with the oldest age value.
     //use filter
     // if value is higher than other values return
-
+let oldest = _.reduce(array, function(accumulator, current){
+    if (current.age > accumulator.age){
+        return current
+    } else {
+        return accumulator
+    }
+})
+return oldest.name
 };
 
 var youngestCustomer = function(array){
 //loop through the array to find the < number on the age key
 //filter 
+let youngest = _.reduce(array, function(accumulator, current){
+    if (current.age > accumulator.age){
+        return accumulator
+    } else {
+        return current
+    }
+})
+return youngest.name
 };
+
 
 var averageBalance = function(array){
 // gather all the balances using filter
@@ -51,15 +76,25 @@ var averageBalance = function(array){
 var firstLetterCount = function(array, letter){
     // use reduce to count the number of names with a given letter
     //use pluck?
+    let letterGiven = _.filter(array, function(customer){
+        if (customer.name[0].toUpperCase() === letter.toUpperCase()){
+            return customer.name;
+        }
+    })
+    return letterGiven
 };
 
 var friendFirstLetterCount = function(array, customer, letter){
 // loop through the friends array of a given customer in the array, check 
 // which how many times a given letter starts a name.
+
 };
 
 var friendsCount = function(array, name){
-
+let customerFriend = _.filter(array, function(customer){
+    return customer.name
+})
+return customerFriend
 };
 
 var topThreeTags = function(array){
@@ -68,7 +103,19 @@ var topThreeTags = function(array){
 
 var genderCount = function(array){
 // use reduce to gather male, female and non-binary
+return _.reduce(array, function(accumulator, current){
+    if (current.gender === "female"){
+        accumulator += 1;
+    } else if (current.gender === "male"){
+       return  accumulator += 1
+    } else if (current.gender === "non-binary"){
+        return accumulator +=1
+    } return accumulator;
+   
+    
+}, 0) 
 };
+
 
 //////////////////////////////////////////////////////////////////////
 // DON'T REMOVE THIS CODE ////////////////////////////////////////////
