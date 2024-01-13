8f63668f6c28ebed4237d989280c525f7e8b4286
Danielle Goldberg <dhgoldberg15@gmail.com>
2024-01-13 18:03:10 +0000

    studies

--- Studies /variables.js
+++ Studies /variables.js
@@ -1,8 +1,16 @@
 /**
  * 0: Variables
- * Variables are the primary way we contain information in Javascript. 
- * 1: Declaration and assignment
+ * Variables are the primary way we contain information in Javascript. Esentialy variables are just lables to encluse data assigned to that lable.
+ * 1: Declaration and assignment. Declaration is how we let the computer know this word is going to be used as a variable. assignment is how we assign data to that lable. 
+ * Declaration consists of a key word and the variable. The assignment then includs the variable name. If we do this in one step it is call inisalization.
+ * 
  * 2: Var, let, const 
+ * These are the three keywords that let the computer know we are encapsulating data with a lable. 
+ * Var, is the oldest of the key words and can be reasigned. It is function or global scoped, this means if it a variable is inishalized with var it is accessable everywhere. 
+ * let 
+ * const
+ * 
+ * 
  * 3: Hoisting
  * 
  * 
