1d76556a62100cdd257ba12c9819cf7436bb2bf2
Danielle Goldberg <dhgoldberg15@gmail.com>
2024-01-19 22:11:35 +0000

    oporators start

--- Studies /oporators.js
+++ Studies /oporators.js
@@ -1,11 +1,12 @@
 /**
- * 0 Oporators:
- * Assignment Oprators 
- * Arithmatic Oporators
- * Comparison Oporators
- * Logical Oporators
- * Unary Oporators
- * Ternary Oporators 
+ * 0 Oporators: These are diffrent symbols and compination of symbols used to tell the computer to preform a certen action. This is a consise way to write out a longer sentience in a writen or spoken language. 
+ * The classic example of oporators for javascript is the use of the equle sign to assign a variable to a datatype. 
+ * 1: Assignment Oprators: These oporators are used to make assignments and preform simple action together in one combined symbol. 
+ * 2: Arithmatic Oporators
+ * 3:Comparison Oporators
+ * 4: Logical Oporators
+ * 5: Unary Oporators
+ * 6: Ternary Oporators 
  * 
  * 
  * 
