a8925173de5fa3aee3efb0c2e988653ec10ead40
Danielle Goldberg <dhgoldberg15@gmail.com>
2024-01-20 00:31:48 +0000

    oporands - code

--- Studies /oporators.js
+++ Studies /oporators.js
@@ -2,10 +2,13 @@
  * 0 Oporators: These are diffrent symbols and compination of symbols used to tell the computer to preform a certen action. This is a consise way to write out a longer sentience in a writen or spoken language. 
  * The classic example of oporators for javascript is the use of the equle sign to assign a variable to a datatype. 
  * 1: Assignment Oprators: These oporators are used to make assignments and preform simple action together in one combined symbol. 
- * 2: Arithmatic Oporators
- * 3:Comparison Oporators
- * 4: Logical Oporators
- * 5: Unary Oporators
+ * 2: Arithmatic Oporators: These are used to preform arithmatic computations on variables but not assign at the same time. Like adding variables together. 
+ * 3:Comparison Oporators: These oporators compaire to sides of the oporand and returns a boolian value based on that comparison. 
+ * Like when seting a condition in an if statment, if the result of the comparison resolves to true then exicute the prosess in the code block. 
+ * Comparison operators allow us to assert the equality of a statement with JavaScript.
+ * 4: Logical Oporators There are situations when we need to assert two condition, similar to comparison but it adds a layer of complexity to the process. T
+ * The logical oporators let us say that two comparisons must be true before something happens or one or the other must me true. 
+ * 5: Unary Oporators A unary operation is an operation with only one operand. This operand comes either before or after the operator.
  * 6: Ternary Oporators 
  * 
  * 
