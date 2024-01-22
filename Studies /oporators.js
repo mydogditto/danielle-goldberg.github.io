a44874348aa3f779c761789329e74fd54d59b548
Danielle Goldberg <dhgoldberg15@gmail.com>
2024-01-22 20:52:00 +0000

    oporators

--- Studies /oporators.js
+++ Studies /oporators.js
@@ -9,31 +9,91 @@
  * 4: Logical Oporators There are situations when we need to assert two condition, similar to comparison but it adds a layer of complexity to the process. T
  * The logical oporators let us say that two comparisons must be true before something happens or one or the other must me true. 
  * 5: Unary Oporators A unary operation is an operation with only one operand. This operand comes either before or after the operator.
- * 6: Ternary Oporators 
+ * 6: Ternary Oporators This is the only oporator that takes three oporands. The condition, what to do if the condition is true, third what to do if the condition is false
  * 
  * 
  * 
  * Code examples 
  * 
- * 
- * 
  */
-// The + operator can be used to concatenate strings.
-let string1 = ""
-string1 = "hello" + " " + "world";
+// Examples of Operators 
+// 1 .Assignment Operators
 
-console.log(string1)// hello world
-// The += operator can be used to concatenate strings.
-string1 += " " + "goodbye";
-console.log(string1)// helo world goodbye
+// simple assignment operator
+let a = 10;
+console.log(a);// 10
+// addition assignment operator
+a += 5;
+console.log(a);// 15
+// subtraction assignment operator
+a -= 5;
+console.log(a);// 10
+// multiplication assignment operator
+a *= 5;
+console.log(a);// 50
+// division assignment operator
+a /= 5;
+console.log(a);// 10
+// modulus assignment operator
+a %= 5;
+console.log(a);// 0
+// exponentiation assignment operator
+let b = 2;
+b **= 5;
+console.log(b);// 32
+// 2. Arithmetic Operators
+// addition operator
+let c = 10;
+let d = 20;
+console.log(c + d);// 30
+// subtraction operator
+console.log(d - c);// 10
+// multiplication operator
+console.log(c * d);// 200
+// division operator
+console.log(d / c);// 2
+// modulus operator
+console.log(d % c);// 0
+// exponentiation operator
+console.log(c ** d);// 10000000000
+// 3. Comparison Operators
+// equal to operator
+let e = 10;
+console.log(e == 10);// true
+// bang  operator
+console.log(e != 10);// false
+// strict equal to operator
+console.log(10 === 10);// true
+// strict not equal to operator
+console.log(9 !== 10);// false
+// greater than operator
+console.log(8 > 10);// false
+// less than operator
+console.log(8 < 10);// true
+// greater than or equal to operator
+console.log(8 >= 10);// false
+// less than or equal to operator
+console.log(8 <= 10);// true
+// 4. Logical Operators
+// logical 'and' operator
+console.log(10 > 9 && 10 > 8);// true
+// logical 'or' operator
+console.log(10 > 9 || 10 > 8);// true
+// logical 'not' operator
+console.log(!(10 > 9));// false
+// 5.  Unary Operators
+// increment operator
+let f = 15;
+f++
+console.log(f);// 16
 
-//The -+ operator can be used to change strings it will return NaN.
-string1 -= "goodbye";
-console.log(string1)// NaN
-// The *= operator can be used to change strings.
-let string2 = "7";
-string2 *= "4";
-console.log(string2)// 28
-// However, the values cannot be mixed unless simply concatenating them.
- string2 *= "world"
-console.log(string2)// NaN
+// decrement operator
+let g = 10;
+g--
+console.log(g);// 9
+//Ternary Operators 
+// condition? true: false
+let h = 10;
+let i = 20;
+h > i ? console.log("h is greater than i") : console.log("i is greater than h");// i is greater than h
+
