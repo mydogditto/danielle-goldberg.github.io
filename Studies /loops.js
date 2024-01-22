8d6aa637dbd3c96d79fcfe453c04b461590f926d
Danielle Goldberg <dhgoldberg15@gmail.com>
2024-01-22 21:30:54 +0000

    loops

--- Studies /loops.js
+++ Studies /loops.js
@@ -1,8 +1,14 @@
 /**
- * Loops in general
- * For loop, while, and for in loop
- * Looping forward x number of times counting up 
- * Looping down counting backward to zero 
+ * 0:Loops Are used to preform repeditive tasks based on a condition evaluating to true and ending when the condition evaluates to false. 
+ * Loops are exstrimly common and useful to do things like accessing every value in an array or object.
+ * 1:For loop, while, and for in loop. These are three main types of loops. The for loop is used on arrays and string. It uses the for keyword It has a start statment. A stop statment and a final exspresstion
+ * The first thing that is done is to inishalize a variable commonly i to zero then a stop statment like when the computor has looped through all the values in the array stop looping. 
+ * Then the final statment is what should be done. In many cases is adding the incramenter or decarmentor oporator. 
+ * A while loop is set up slightly diffrently. It uses the while keyword but only after inishalizing the incramenting variable. After the while keyword come the exsprestion and what each itteration should do. 
+ * Looping forward x number of times counting up. One way loops are used is to count up to a number spesified in the loop decloration. For example, if you wanted to loop from 0 to 10 using a for loop you would simple set the second statment to 10. 
+ * 
+ * Looping down counting backward to zero Loops can go either forward or backward. Looping backward involves changing the order of the statments in the decloration. 
+ * A for in loop works the same whay as the for loop but its purpus is to iteratie through objects not arrays or strings. 
  * Looping over an array 
  * Looping over an object
  * 
